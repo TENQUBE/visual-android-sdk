25929c39b62cdc72462849b9da2c226dbbda294e
tenqube <kj.sa@tenqube.com>
2022-07-19 10:56:31 +0900

    #14 transaction join 함수 추가 완료

--- visualbase/src/main/java/com/tenqube/visualbase/domain/transaction/TransactionRepository.kt
+++ visualbase/src/main/java/com/tenqube/visualbase/domain/transaction/TransactionRepository.kt
@@ -1,10 +1,14 @@
 package com.tenqube.visualbase.domain.transaction
 
+import com.tenqube.visualbase.service.transaction.dto.TransactionFilter
+
 interface TransactionRepository {
 
     suspend fun findById(id: String): Result<Transaction>
 
     suspend fun findAll(): Result<List<Transaction>>
+
+    suspend fun findByFilter(filter: TransactionFilter): Result<List<Transaction>>
 
     suspend fun save(item: Transaction): Result<Transaction>
 
--- visualbase/src/main/java/com/tenqube/visualbase/service/transaction/TransactionAppService.kt
+++ visualbase/src/main/java/com/tenqube/visualbase/service/transaction/TransactionAppService.kt
@@ -1,9 +1,13 @@
 package com.tenqube.visualbase.service.transaction
 
+import com.tenqube.visualbase.domain.card.Card
 import com.tenqube.visualbase.domain.card.CardRepository
+import com.tenqube.visualbase.domain.category.Category
 import com.tenqube.visualbase.domain.category.CategoryRepository
+import com.tenqube.visualbase.domain.transaction.Transaction
 import com.tenqube.visualbase.domain.transaction.TransactionRepository
 import com.tenqube.visualbase.domain.transaction.command.SaveTransactionDto
+import com.tenqube.visualbase.domain.usercategoryconfig.UserCategoryConfig
 import com.tenqube.visualbase.domain.usercategoryconfig.UserCategoryConfigRepository
 import com.tenqube.visualbase.service.transaction.dto.JoinedTransaction
 import com.tenqube.visualbase.service.transaction.dto.TransactionFilter
@@ -13,17 +17,63 @@ class TransactionAppService(
     private val transactionRepository: TransactionRepository,
     private val cardRepository: CardRepository,
     private val categoryRepository: CategoryRepository,
-    private val userAppService: UserAppService,
     private val userCategoryConfigRepository: UserCategoryConfigRepository
 ) {
     suspend fun getTransactions(filter: TransactionFilter): Result<List<JoinedTransaction>> {
-        val user = userAppService.getUser()
-        val transactions = transactionRepository.findAll().getOrDefault(listOf())
-        val cards = cardRepository.findAll().getOrDefault(listOf())
-        val categories = categoryRepository.findAll().getOrDefault(listOf())
-        val userCategories = userCategoryConfigRepository.findAll().getOrDefault(listOf())
+        val transactions = transactionRepository
+            .findByFilter(filter)
+            .getOrDefault(listOf())
+        val cards = cardRepository
+            .findAll()
+            .getOrDefault(listOf())
+        val categories = categoryRepository
+            .findAll()
+            .getOrDefault(listOf())
+        val userCategories = userCategoryConfigRepository
+            .findAll()
+            .getOrDefault(listOf())
+        val results = joinTransactions(
+            transactions,
+            cards,
+            categories,
+            userCategories
+        )
+        return Result.success(results)
+    }
 
-        return Result.success(listOf())
+    private fun joinTransactions(
+        transactions: List<Transaction>,
+        cards: List<Card>,
+        categories: List<Category>,
+        userCategories: List<UserCategoryConfig>
+    ): List<JoinedTransaction> {
+        val cardMap = cards.associateBy { it.id }
+        val categoryMap = categories.associateBy { it.id }
+        val userCateMap = userCategories.associateBy { it.id }
+
+        return transactions.mapNotNull {
+            val card = cardMap[it.cardId]
+            val category = categoryMap[it.categoryId]
+            val userCategory = userCateMap[it.userCategoryConfigId]
+            if(hasMandatory(card, category, userCategory)) {
+                JoinedTransaction(
+                    it,
+                    card!!,
+                    category!!,
+                    userCategory!!
+                )
+            } else {
+                null
+            }
+        }
+    }
+
+    private fun hasMandatory(
+        card: Card?,
+        category: Category?,
+        userCategory: UserCategoryConfig?
+    ): Boolean {
+        return card != null && category != null && userCategory != null
     }
 
     suspend fun saveTransactions(transactions: List<SaveTransactionDto>): Result<Unit> {
