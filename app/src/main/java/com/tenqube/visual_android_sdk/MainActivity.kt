e0cbf3ae15eff4fdb7b227c7b24319591b5df7e0
sagwangjin <kj.sa@tneuqbe.com>
2022-07-21 17:19:03 +0900

    디비 연동 퍼미션 로직 추가 완료

--- app/src/main/java/com/tenqube/visual_android_sdk/MainActivity.kt
+++ app/src/main/java/com/tenqube/visual_android_sdk/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.tenqube.visual_android_sdk
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.tenqube.ibk.Layer
 import com.tenqube.ibk.UserArg
 import com.tenqube.ibk.VisualGender
@@ -13,6 +17,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val jbButton: Button = findViewById(R.id.jb)
+
+        checkPermission()
+
         jbButton.setOnClickListener {
             VisualServiceBuilder()
                 .activity(this)
@@ -20,6 +27,30 @@ class MainActivity : AppCompatActivity() {
                 .layer(Layer.PROD)
                 .build()
                 .start(UserArg("uid", 1987, VisualGender.MALE))
+        }
+    }
+
+    private fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this@MainActivity,
+                Manifest.permission.READ_SMS
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this@MainActivity,
+                    Manifest.permission.READ_SMS
+                )
+            ) {
+            } else {
+                ActivityCompat.requestPermissions(
+                    this@MainActivity,
+                    arrayOf(Manifest.permission.READ_SMS, Manifest.permission.RECEIVE_SMS),
+                    0
+                )
+            }
         }
     }
 }
